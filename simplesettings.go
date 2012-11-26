38d21b84a6bd7cd1ad7e29359923eec46372e7e7
Bryan Matsuo <bryan.matsuo@gmail.com>
2012-11-25 16:44:52 -0800

    Empty project generated by gonew.

---
+++ simplesettings.go
@@ -0,0 +1,16 @@
+// Copyright 2012, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*  Filename:    simplesettings.go
+ *  Author:      Bryan Matsuo <bryan.matsuo [at] gmail.com>
+ *  Created:     2012-11-25 16:44:51.995426 -0800 PST
+ *  Description: Main source file in go-simplesettings
+ */
+
+
+// Package simplesettings does ....
+package simplesettings
+
+import (
+)
---
+++ simplesettings_test.go
@@ -0,0 +1,21 @@
+// Copyright 2012, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package simplesettings
+
+/*  Filename:    simplesettings_test.go
+ *  Author:      Bryan Matsuo <bryan.matsuo [at] gmail.com>
+ *  Created:     2012-11-25 16:44:51.995633 -0800 PST
+ *  Description: Main test file for go-simplesettings
+ */
+
+import (
+    "testing"
+)
+
+
+func TestSimplesettings(t *testing.T) {
+
+}
+
